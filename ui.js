16fb209d930c44618104722092412888acfc7fbb
Steven Woodward <steveanthony999@gmail.com>
2020-05-14 01:35:18 -0700

    added node for later use, dotenv, mapquest api for reverse geocoding

--- ui.js
+++ ui.js
@@ -1,6 +1,5 @@
 class UI {
   constructor() {
-    this.cityName = document.getElementById('city-name');
     this.temp = document.getElementById('tempurature');
     this.feelsLike = document.getElementById('feels-like');
     this.low = document.getElementById('low');
@@ -56,14 +55,36 @@ class UI {
         : 'NW';
     }
 
-    this.cityName.innerHTML = weather.name;
+    function getCity(lat, long) {
+      const key = '';
+
+      const xhr = new XMLHttpRequest();
+
+      xhr.open(
+        'GET',
+        `http://www.mapquestapi.com/geocoding/v1/reverse?key=${key}&location=${lat},${long}`,
+        true
+      );
+
+      xhr.onload = function() {
+        if (this.status === 200) {
+          const response = JSON.parse(this.responseText);
+
+          document.getElementById('city-name').innerHTML =
+            response.results[0].locations[0].adminArea5;
+        }
+      };
+      xhr.send();
+    }
+
+    getCity(weather.lat, weather.lon);
     this.temp.innerHTML = Math.round(weather.current.temp) + 'º';
     this.feelsLike.innerHTML = Math.round(weather.current.feels_like) + 'º';
     this.low.innerHTML = Math.round(weather.daily[0].temp.min) + 'º';
     this.high.innerHTML = Math.round(weather.daily[0].temp.max) + 'º';
     this.humidity.innerHTML = weather.current.humidity + '%';
     this.skies.innerHTML = weather.current.weather[0].description;
-    this.wind.innerHTML = weather.current.wind_speed;
+    this.wind.innerHTML = Math.round(weather.current.wind_speed) + ' mph - ';
     this.windDirection.innerHTML = compass(weather.current.wind_deg);
     this.sunrise.innerHTML = timeStamp(weather.current.sunrise);
     this.sunset.innerHTML = timeStamp(weather.current.sunset);
